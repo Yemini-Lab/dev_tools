d3ae1b0f9f7688e73f8a3f6dda434b0868e07c9d
Kevin Rusch <SEP271905@protonmail.com>
2024-08-07 14:15:13 -0400

    minor path fix

--- utilities/script_compiler.py
+++ utilities/script_compiler.py
@@ -20,7 +20,7 @@ def list_python_files(path):
 
 # Define directories
 os_platform = platform.system().lower()
-npal_directory = Path('/Users/yemini-lab/Documents/GitHub')
+npal_directory = Path('/Users/yemini-lab/Documents/GitHub/NeuroPAL_ID')
 script_directory = npal_directory / '+Wrapper'
 distribution_directory = script_directory / 'dist'
 compile_directory = npal_directory / f"{os_platform[:3]}_visualize" / 'for_redistribution_files_only' / 'lib' / 'bin' / os_platform
