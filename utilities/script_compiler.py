6b9f6e3ed3c2755ef6f6aea5a81e989dfd0cb60f
Kevin Rusch (LabPC) <kwrusch@seas.upenn.edu>
2024-08-07 13:59:23 -0400

    script_compiler cleaning

--- utilities/script_compiler.py
+++ utilities/script_compiler.py
@@ -1,7 +1,10 @@
 import os
+import platform
 import subprocess
 from pathlib import Path
+import shutil
 
+# List of hidden imports
 hidden_imports = [
     'xml.etree',
     'xml.etree.ElementTree',
@@ -9,18 +12,17 @@ hidden_imports = [
     'mx.DateTime'
 ]
 
-
 def list_python_files(path):
+    """List all Python files in the given directory."""
     files = os.listdir(path)
     python_files = [file for file in files if file.endswith('.py')]
     return python_files
-
 
 # Define directories using Path
 npal_directory = Path('C:/Users/YeminiPC/Documents/GitHub/NeuroPAL_ID')
 script_directory = npal_directory / '+Wrapper'
 distribution_directory = script_directory / 'dist'
-compile_directory = npal_directory.parent.parent / 'win_visualize' / 'for_redistribution_files_only' / 'lib'
+compile_directory = npal_directory / 'win_visualize' / 'for_redistribution_files_only' / 'lib' / 'bin' / platform.system().lower()
 
 # List Python files in the directory
 python_files = list_python_files(script_directory)
@@ -34,16 +36,19 @@ for file in python_files:
     cmd = f"pyinstaller {file_path}"
 
     # Add hidden imports to the command
-    hidden_imports_string = ' '.join(f"--hidden-import={eachImport[:-3]}" for eachImport in hidden_imports)
+    hidden_imports_string = ' '.join(f"--hidden-import={import_name}" for import_name in hidden_imports)
     cmd += f" {hidden_imports_string} --onefile"
 
-    print(cmd)
-
+    # Execute the command
     subprocess.call(cmd, shell=True)
 
-    # Define source and destination directories for moving files
-    source_dir = distribution_directory
-    destination_dir = compile_directory
+    # Define source and destination paths for moving files
+    source_file = distribution_directory / file.replace('.py', '.exe')
+    destination_file = compile_directory / file.replace('.py', '.exe')
 
     # Move files from source to destination
-    print(f"\nMove from {source_dir} to {destination_dir}\n\n")
+    try:
+        shutil.move(str(source_file), str(destination_file))
+        print(f"Successfully compiled {destination_file}.\n")
+    except FileNotFoundError:
+        print(f"ERROR: Compilation failed for {source_file}...\n")
