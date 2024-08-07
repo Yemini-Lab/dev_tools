bbdcc1e5cb74dc88dc289379d6450dc117e86a09
Kevin Rusch <SEP271905@protonmail.com>
2024-08-07 17:43:56 -0400

    added data file search

--- utilities/script_compiler.py
+++ utilities/script_compiler.py
@@ -1,25 +1,34 @@
 import os
+import h5py
+import pynwb
+import shutil
 import platform
 import subprocess
 from pathlib import Path
-import shutil
 
-import pynwb
-import h5py
 
-# List of hidden imports
-hidden_imports = [
-    'xml.etree',
-    'xml.etree.ElementTree',
-    'scikit-image',
-    'mx.DateTime',
-    'h5py.defs',
-    'h5py.utils',
-    'h5py.h5ac',
-    'h5py._proxy'
-]
+def clear_cache(path):
+    """Deletes all pyinstaller-related cache files."""
+    for root, dirs, files in os.walk(path, topdown=False):
+        for file in files:
+            if file.endswith('.spec'):
+                file_path = os.path.join(root, file)
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
 
-hidden_paths = []
+        for dir_name in dirs:
+            if dir_name in ('build', 'dist', '__pycache__'):
+                dir_path = os.path.join(root, dir_name)
+                shutil.rmtree(dir_path)
+                print(f"Deleted directory: {dir_path}")
+
+
+def list_data_files(path):
+    """Lists all data files paths for given library path."""
+    files = listdir(path)
+    data_files = [os.path.join(path, file) for file in files if not file.endswith(['.py', 'pyc', 'pyd'])]
+    return data_files
+
 
 def list_python_files(path):
     """List all Python files in the given directory."""
@@ -27,37 +36,52 @@ def list_python_files(path):
     python_files = [file for file in files if file.endswith('.py')]
     return python_files
 
+
 # Define directories
 os_platform = platform.system().lower().replace('darwin', 'macos')
 
 if os_platform == 'windows':
-    npal_directory = Path('C:\\Users\\YeminiPC\\Documents\\GitHub\\NeuroPAL_ID')
+    npal_directory = Path(
+        f"C:\\Users\\{os.environ.get('USER', os.environ.get('USERNAME'))}\\Documents\\GitHub\\NeuroPAL_ID")
 else:
-    npal_directory = Path('/Users/yemini-lab/Documents/GitHub/NeuroPAL_ID')
+    npal_directory = Path(f"/Users/{os.environ.get('USER', os.environ.get('USERNAME'))}/Documents/GitHub/NeuroPAL_ID")
 
 script_directory = npal_directory / '+Wrapper'
 distribution_directory = Path('.') / 'dist'
 compile_directory = npal_directory / f"{os_platform[:3]}_visualize" / 'for_redistribution_files_only' / 'lib' / 'bin' / os_platform
 
+# List of known hidden imports
+hidden_imports = ['xml.etree', 'xml.etree.ElementTree', 'scikit-image', 'mx.DateTime', 'h5py.defs', 'h5py.utils',
+                  'h5py.h5ac', 'h5py._proxy']
+hidden_paths = [script_directory]
+data_files = []
+
 # List Python files in the directory
 python_files = list_python_files(script_directory)
 
-# Populate hidden paths
-hidden_paths.extend([str(script_directory), str(Path(pynwb.__file__).parent), str(Path(h5py.__file__).parent)])
+# Populate contingency lists
+schema_libraries = [pynwb, h5py, hdmf]
+
+for eachLib in schema_libraries:
+    lib_path = Path(eachLib.__file__).parent
+    hidden_paths.extend([str(lib_path)])
+    data_files.extend(list_data_files(lib_path))
+
+# Formulate arguments
+hidden_imports_string = ' '.join(f"--hidden-import={import_name}" for import_name in hidden_imports)
+paths_string = ' '.join(f"--paths={script_directory / local_file}" for local_file in python_files)
+data_string = ' '.join(f"--add-data={data_file}" for data_file in data_files)
 
 # Process each Python file
 for file in python_files:
     file_path = script_directory / file
-    # Add hidden imports to the command
-    hidden_imports_string = ' '.join(f"--hidden-import={import_name}" for import_name in hidden_imports)
-    paths_string = ' '.join(f"--paths={script_directory / local_file}" for local_file in python_files)
-    cmd = f"pyinstaller {paths_string} {file_path} {hidden_imports_string} --onefile"
+
+    cmd = f"pyinstaller {paths_string} {data_string} {file_path} {hidden_imports_string} --onefile"
 
     if os_platform == 'macos':
         subprocess.call('alias pip=pip3', shell=True)
 
     # Execute the command
-    print(cmd)
     subprocess.call(cmd, shell=True)
 
     # Define source and destination paths for moving files
