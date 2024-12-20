27aad32e04829f3236950f0e2ab0fad1cb447d29
Kevin Rusch (LabPC) <kwrusch@seas.upenn.edu>
2024-12-20 17:50:06 -0500

    fix diagnostic summary construction

--- scripts/support_library/nwb/validation.py
+++ scripts/support_library/nwb/validation.py
@@ -1,4 +1,6 @@
 import math
+
+import numpy as np
 import pynwb.misc
 import pynwb.ophys
 import hdmf.common.table
@@ -32,16 +34,16 @@ def validate_acquisitions(nwb_obj):
 
     for each_acquisition_module in acquisition_modules:
         module_obj = nwb_obj.acquisition[each_acquisition_module]
-        if module_obj.imaging_volume.grid_spacing[:] != expected_grid_spacing:
+        if any(module_obj.imaging_volume.grid_spacing[:] != expected_grid_spacing):
             issue_list.append(f"{each_acquisition_module} grid_spacing")
 
         channels = module_obj.imaging_volume.optical_channel
         for each_channel in channels:
             if (
                     each_channel.name not in expected_channels
-                    or each_channel.emission_range[:] == [0, 0]
+                    or any(each_channel.emission_range[:] == [0, 0])
                     or each_channel.emission_lambda == 0
-                    or each_channel.excitation_range[:] == [0, 0]
+                    or any(each_channel.excitation_range[:] == [0, 0])
                     or each_channel.excitation_lambda == 0
             ):
                 issue_list.append(f"{each_acquisition_module} channel: {each_channel.name}")
@@ -77,12 +79,12 @@ def validate_processed(nwb_obj):
 
             elif isinstance(each_child, pynwb.ophys.ImageSegmentation):
                 if each_child.name == 'TrackedNeurons':
-                    tracked_rois = each_child['TrackedNeuronROIs'].voxel_mask
-                    if tracked_rois.ndim < 3:
+                    tracked_rois = np.array(each_child['TrackedNeuronROIs'].voxel_mask[:].tolist())
+                    if tracked_rois.shape[1] < 3:
                         issue_list.append(f"{module_name} {interface_name} ROI compressed along time dimension")
 
                     non_zero = (tracked_rois > 0).any(axis=(0, 1))
-                    if not all(non_zero):
+                    if not non_zero:
                         issue_list.append(f"{module_name} {interface_name} ROI has all-zero slices along dimensions")
 
             else:
@@ -96,15 +98,15 @@ def validate(nwb_obj):
 
     subject_issues = validate_subject(nwb_obj)
     if len(subject_issues) > 0:
-        issue_list.append(subject_issues)
+        issue_list += subject_issues
 
     acquisition_issues = validate_acquisitions(nwb_obj)
     if len(acquisition_issues) > 0:
-        issue_list.append(acquisition_issues)
+        issue_list += acquisition_issues
 
     processing_issues = validate_processed(nwb_obj)
     if len(processing_issues) > 0:
-        issue_list.append(processing_issues)
+        issue_list += processing_issues
 
     is_valid = len(issue_list) == 0
     if is_valid:
